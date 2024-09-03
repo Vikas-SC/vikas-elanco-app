21615027d3b97253a92417ed94539203ef53ce92
vikas s c <vikas_shetty@condenast.com>
2024-09-03 17:01:14 +0530

    Setup nextjs application

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const Dashboard: FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is a TypeScript project setup.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
